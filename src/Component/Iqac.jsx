61498ec91e048995317c894a69ef013332b3b649
Ankit626517 <ankitrathore48132@gmail.com>
2024-09-30 10:49:50 +0530

    kuchh change huva

--- src/Component/Iqac.jsx
+++ src/Component/Iqac.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Iqac() {
   return (
     <div>
-    <h1>garam hai sabji thandi hai roti 
-      or bta kya haal hai moti
+    <h1>ajfjhfhsjkfhhjksdf
+      sdfksdfdhsdjf
+      sddfjsdhfjk
 
     </h1>
     </div>
