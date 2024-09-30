195e359ae558df7ac6616f85f6d6f1e1752520cd
Ankit626517 <ankitrathore48132@gmail.com>
2024-09-30 10:47:53 +0530

    moti ne kuchh kara

--- src/Component/Iqac.jsx
+++ src/Component/Iqac.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Iqac() {
   return (
     <div>
-      <h1>HEllo i am IQAC</h1>
+    <h1>garam hai sabji thandi hai roti 
+      or bta kya haal hai moti
+
+    </h1>
     </div>
   )
 }
